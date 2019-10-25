2f9b32dd0eb9490c3a3b15c808d23cc52dd804db
VICTORALI1 <VICTORALI1>
2019-10-25 14:19:05 +0300

    Completed module

---
+++ Practice.py
@@ -0,0 +1,11 @@
+nums = [1, 2, 3, 4, 5]
+
+
+def intro(**data):
+    print("\nData type of argument:", type(data))
+    for key, value in data.items():
+        print("{} : {}".format(key, value))
+
+
+intro(Firstname="Sita", Lastname="Sharma", Age=22, Phone=1234567890)
+intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)
