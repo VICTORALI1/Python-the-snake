ed4f4aa315dea2f9c11b06dc8d093e47d11cfb99
VICTORALI1 <VICTORALI1>
2019-10-28 11:31:36 +0300

    learning operating system functionality

--- Practice.py
+++ Practice.py
@@ -1,11 +1,26 @@
+import os
 nums = [1, 2, 3, 4, 5]
 
-
-def intro(**data):
+'''def intro(**data):
     print("\nData type of argument:", type(data))
     for key, value in data.items():
         print("{} : {}".format(key, value))
 
 
 intro(Firstname="Sita", Lastname="Sharma", Age=22, Phone=1234567890)
-intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)
+intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)'''
+
+
+'''def leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+print(leap_year(2100))'''
+'''print(os.getcwd())
+
+os.makedirs('OS-DEMO-2/Sub-Dir-1')
+print(os.listdir())'''
+
+
+
+
